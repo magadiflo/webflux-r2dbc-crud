f058ddb15cf6249ee4a94506d0869311cc7bf4d1
Martín <magadiflo@gmail.com>
2024-02-16 17:24:29 -0500

    Agregando utilidades

---
+++ src/main/java/dev/magadiflo/r2dbc/app/web/advice/ApiAdvice.java
@@ -0,0 +1,19 @@
+package dev.magadiflo.r2dbc.app.web.advice;
+
+import dev.magadiflo.r2dbc.app.exception.ApiException;
+import dev.magadiflo.r2dbc.app.web.util.ResponseMessage;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
+public class ApiAdvice {
+    @ExceptionHandler(ApiException.class)
+    public Mono<ResponseEntity<ResponseMessage<Void>>> exception(ApiException exception) {
+        ResponseMessage<Void> message = ResponseMessage.<Void>builder()
+                .message(exception.getMessage())
+                .build();
+        return Mono.just(new ResponseEntity<>(message, exception.getHttpStatus()));
+    }
+}
