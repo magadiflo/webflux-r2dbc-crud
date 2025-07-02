011d83b94df6eddfb3cc5f4957314203af55784c
Martín Díaz <magadiflo@gmail.com>
2025-07-02 11:34:42 -0500

    Construyendo consulta dinámica con criterios de búsqueda y paginación - parte 2

--- src/test/java/dev/magadiflo/r2dbc/app/integration/BookAuthorDaoImplTest.java
+++ src/test/java/dev/magadiflo/r2dbc/app/integration/BookAuthorDaoImplTest.java
@@ -2,6 +2,7 @@ package dev.magadiflo.r2dbc.app.integration;
 
 import dev.magadiflo.r2dbc.app.dao.BookAuthorDao;
 import dev.magadiflo.r2dbc.app.dao.impl.BookAuthorDaoImpl;
+import dev.magadiflo.r2dbc.app.dto.BookCriteria;
 import dev.magadiflo.r2dbc.app.entity.BookAuthor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import reactor.test.StepVerifier;
 
 import java.time.LocalDate;
@@ -192,6 +195,88 @@ class BookAuthorDaoImplTest extends AbstractTest {
     }
 
     @Test
-    void findAllToPage() {
+    void shouldReturnBookProjectionsFilteredByQuery() {
+        var criteria = new BookCriteria("ri", null);
+        Pageable pageable = PageRequest.of(0, 5);
+        this.bookAuthorDao.findAllToPage(criteria, pageable)
+                .doOnNext(bookProjection -> log.info("{}", bookProjection))
+                .as(StepVerifier::create)
+                .assertNext(bookProjection -> {
+                    Assertions.assertEquals("El zorro de arriba y el zorro de abajo", bookProjection.title());
+                    Assertions.assertEquals(LocalDate.parse("2002-05-06"), bookProjection.publicationDate());
+                    Assertions.assertFalse(bookProjection.onlineAvailability());
+                    Assertions.assertNull(bookProjection.authors());
+                    Assertions.assertTrue(bookProjection.authorNames().isEmpty());
+                })
+                .assertNext(bookProjection -> {
+                    Assertions.assertEquals("La ciudad y los perros", bookProjection.title());
+                    Assertions.assertEquals(LocalDate.parse("1985-03-18"), bookProjection.publicationDate());
+                    Assertions.assertTrue(bookProjection.onlineAvailability());
+                    Assertions.assertNotNull(bookProjection.authors());
+                    Assertions.assertFalse(bookProjection.authorNames().isEmpty());
+                })
+                .verifyComplete();
     }
+
+    @Test
+    void shouldReturnBookProjectionsFilteredByPublicationDate() {
+        var criteria = new BookCriteria("", LocalDate.parse("1988-07-15"));
+        Pageable pageable = PageRequest.of(0, 5);
+        this.bookAuthorDao.findAllToPage(criteria, pageable)
+                .doOnNext(bookProjection -> log.info("{}", bookProjection))
+                .as(StepVerifier::create)
+                .assertNext(bookProjection -> {
+                    Assertions.assertEquals("Redoble por Rancas", bookProjection.title());
+                    Assertions.assertEquals(LocalDate.parse("1988-07-15"), bookProjection.publicationDate());
+                    Assertions.assertTrue(bookProjection.onlineAvailability());
+                    Assertions.assertNull(bookProjection.authors());
+                    Assertions.assertTrue(bookProjection.authorNames().isEmpty());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnBookProjectionsFilteredByQueryAndPublicationDate() {
+        var criteria = new BookCriteria("ciu", LocalDate.parse("1985-03-18"));
+        Pageable pageable = PageRequest.of(0, 5);
+        this.bookAuthorDao.findAllToPage(criteria, pageable)
+                .doOnNext(bookProjection -> log.info("{}", bookProjection))
+                .as(StepVerifier::create)
+                .assertNext(bookProjection -> {
+                    Assertions.assertEquals("La ciudad y los perros", bookProjection.title());
+                    Assertions.assertEquals(LocalDate.parse("1985-03-18"), bookProjection.publicationDate());
+                    Assertions.assertTrue(bookProjection.onlineAvailability());
+                    Assertions.assertNotNull(bookProjection.authors());
+                    Assertions.assertFalse(bookProjection.authorNames().isEmpty());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldReturnBookProjectionsWithoutFilter() {
+        var criteria = new BookCriteria(" ", null);
+        Pageable pageable = PageRequest.of(0, 5);
+        this.bookAuthorDao.findAllToPage(criteria, pageable)
+                .doOnNext(bookProjection -> log.info("{}", bookProjection))
+                .as(StepVerifier::create)
+                .assertNext(bookProjection -> {
+                    Assertions.assertEquals("El zorro de arriba y el zorro de abajo", bookProjection.title());
+                    Assertions.assertEquals(LocalDate.parse("2002-05-06"), bookProjection.publicationDate());
+                    Assertions.assertFalse(bookProjection.onlineAvailability());
+                    Assertions.assertNull(bookProjection.authors());
+                    Assertions.assertTrue(bookProjection.authorNames().isEmpty());
+                })
+                .expectNextCount(1)
+                .assertNext(bookProjection -> {
+                    Assertions.assertEquals("Los ríos profundos", bookProjection.title());
+                    Assertions.assertEquals(LocalDate.parse("1999-01-15"), bookProjection.publicationDate());
+                    Assertions.assertTrue(bookProjection.onlineAvailability());
+                    Assertions.assertNotNull(bookProjection.authors());
+                    Assertions.assertFalse(bookProjection.authorNames().isEmpty());
+                    Assertions.assertEquals(2, bookProjection.authorNames().size());
+                })
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
 }
